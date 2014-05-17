3bae951eccba6aaccd8ca101bef1b91c8719c7bd
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-05-17 04:08:39 +0300

    sum

--- glfw.hs
+++ glfw.hs
@@ -1,39 +1,40 @@
 import Data.Maybe
 import System.Exit
 import Graphics.Rendering.OpenGL as GL
-import qualified Graphics.UI.GLFW as G
+import qualified Graphics.UI.GLFW as GLFW
 
 main = do
-  G.init
-  mw <- G.createWindow 400 400 "title" Nothing Nothing
+  GLFW.init
+  mw <- GLFW.createWindow 400 400 "title" Nothing Nothing
   case mw of
     Nothing -> do
           err "Can't create window"
           exitWith (ExitFailure 0)
     Just m  -> do
-          G.makeContextCurrent mw
+          GLFW.makeContextCurrent mw
           GL.clearColor $= Color4 1 0 0 0
-          G.setWindowCloseCallback m (Just exit')
-          start m
-	
-exit' w = exitWith ExitSuccess
+          run m
 
 err e = print e
 
-start :: G.Window -> IO ()
-start mw = do
-  GL.clear [GL.ColorBuffer, GL.DepthBuffer]
-  render
-  G.swapBuffers mw
-  GL.flush
-  G.pollEvents
-  esc <- G.getKey mw G.Key'Escape
-  if esc == G.KeyState'Pressed
-  then G.terminate
-  else start mw
+isEscape mw = do
+  esc <- GLFW.getKey mw GLFW.Key'Escape
+  return (esc == GLFW.KeyState'Pressed)
 
-render =
+runIfTrue v1 v2 fail succ = if v1 || v2 then fail else succ
+
+run :: GLFW.Window -> IO ()
+run mw = do
+  render
+  GLFW.swapBuffers mw
+  GLFW.pollEvents
+  esc <- isEscape mw
+  wsClose <- GLFW.windowShouldClose mw
+  runIfTrue esc wsClose GLFW.terminate (run mw)
+
+render = do
+  GL.clear [GL.ColorBuffer, GL.DepthBuffer]
   GL.renderPrimitive GL.Points $ do
+    GL.vertex $ GL.Vertex2 (-0.5 :: GLfloat) (0.0 :: GLfloat)
     GL.vertex $ GL.Vertex2 (0.0 :: GLfloat) (0.0 :: GLfloat)
     GL.vertex $ GL.Vertex2 (0.5 :: GLfloat) (0.0 :: GLfloat)
-    GL.vertex $ GL.Vertex2 (-0.5 :: GLfloat) (0.0 :: GLfloat)
