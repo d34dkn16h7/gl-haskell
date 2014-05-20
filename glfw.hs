982ae12a1809e75d998d9c379c74289c9877658a
Stanislav Ursache <d34dkn16h7@gmail.com>
2014-05-20 15:18:46 +0300

    asdd

--- glfw.hs
+++ glfw.hs
@@ -48,8 +48,8 @@ gInput GameWorld {win = m_win , cam = m_cam} = do
   down  <- GLFW.getKey m_win GLFW.Key'Down
   let x = gX' m_cam
   let y = gY' m_cam
-  let nX = if left == GLFW.KeyState'Pressed then x - (0.001 :: GLfloat) else if right == GLFW.KeyState'Pressed then x + (0.001 :: GLfloat) else x 
-  let nY = if down == GLFW.KeyState'Pressed then y - (0.001 :: GLfloat) else if up == GLFW.KeyState'Pressed then y + (0.001 :: GLfloat) else y 
+  let nX = if left == GLFW.KeyState'Pressed then x - 0.001 else if right == GLFW.KeyState'Pressed then x + 0.001 else x 
+  let nY = if down == GLFW.KeyState'Pressed then y - 0.001 else if up == GLFW.KeyState'Pressed then y + 0.001 else y 
   return  (GameWorld m_win $ (Camera $ V2 nX nY))
 
 render :: Camera -> IO ()
@@ -64,9 +64,9 @@ draw = do
   renderQuad (-1) 0
 
 renderQuad x y = do
-    GL.color $ Color4 (1 :: GLfloat) (1 :: GLfloat) (1 :: GLfloat) (1 :: GLfloat)
-    GL.vertex $ GL.Vertex2 (-0.5 + x :: GLfloat) (-0.5 + y :: GLfloat)
-    GL.vertex $ GL.Vertex2 (-0.5 + x :: GLfloat) (0.5 + y :: GLfloat)
-    GL.color $ Color4 (0.5 :: GLfloat) (0.5 :: GLfloat) (0.5 :: GLfloat) (0.5 :: GLfloat)
-    GL.vertex $ GL.Vertex2 (0.5 + x :: GLfloat) (0.5 + y :: GLfloat)
-    GL.vertex $ GL.Vertex2 (0.5 + x :: GLfloat) (-0.5 + y :: GLfloat)
+    GL.color $ Color4 1 1 1 (1 :: GLfloat)
+    GL.vertex $ GL.Vertex2 (-0.5 + x) (-0.5 + y :: GLfloat)
+    GL.vertex $ GL.Vertex2 (-0.5 + x) (0.5 + y :: GLfloat)
+    GL.color $ Color4 0.5 0.5 0.5 (0.5 :: GLfloat)
+    GL.vertex $ GL.Vertex2 (0.5 + x) (0.5 + y :: GLfloat)
+    GL.vertex $ GL.Vertex2 (0.5 + x) (-0.5 + y :: GLfloat)
